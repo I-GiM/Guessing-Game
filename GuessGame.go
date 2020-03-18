136d8a6e74a3c0afc0344898444cca1ce86f8e5b
ikennaoyiih@gmail.com <42140997+IkennaC@users.noreply.github.com>
2020-03-18 01:08:34 +0100

    Can your guess save your life?

--- GuessGame.go
+++ GuessGame.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	fmt.Println("Guess the number")
-	fmt.Println("Please input your guess")
+	fmt.Println("Can Your Guess Save Your Life!?")
+	println()
 
 	//generate a random number
 	source := rand.NewSource(time.Now().UnixNano())
@@ -16,7 +16,29 @@ func main() {
 	secretNumber := randomizer.Intn(10)
 
 	var guess int
-	fmt.Scan(&guess)
+	/*fmt.Scan(&guess)
 
 	fmt.Printf("You guessed %d\n", guess)
+
+	if guess == secretNumber {
+		fmt.Println("Subarashi! You live!")
+	} else {
+		fmt.Println("Omae wa mo shindeiru")
+	}*/
+	for i := 0; i < 3; i++ {
+		fmt.Println("Input your guess")
+		fmt.Scan(&guess)
+		if guess == secretNumber {
+			fmt.Println("Subarashi! You Live!!!")
+			break
+		}
+		if guess != secretNumber {
+			fmt.Println("Wrong! Try again")
+			println()
+		}
+		if i == 2 {
+			fmt.Println("Wrong!!! Omae wa mo shindeiru")
+			println()
+		}
+	}
 }
